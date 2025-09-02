e8d9482b6420e1a508e2ba148e69cbb103c70b26
PujithaNandipati <162285733+PujithaNandipati@users.noreply.github.com>
2025-09-02 21:09:44 +0530

    Update projectService.js

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = 'http://localhost:8080/api/projects';
+const API = 'https://portfoliobackend-5.onrender.com/api/projects';
 
 export const getAll = () => axios.get(API);
 
